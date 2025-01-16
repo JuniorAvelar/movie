37fa9c647a9d328f36c3570b309d4b0de5cd5827
Edivaldo Junior <junioravelar853@gmail.com>
2025-01-16 10:06:33 -0300

    configurando card, com infomaçoes da api

--- main/js/script.js
+++ main/js/script.js
@@ -14,8 +14,8 @@ const tmdbApi = axios.create({
     }
 }
 )
-
-
+ 
+// function que busca o filme e a série 
 const fetchMoviesAndSeries = async (MovieName) => {
     try {
         const [filme , serie] = await axios.all([
@@ -30,6 +30,61 @@ const fetchMoviesAndSeries = async (MovieName) => {
         console.log("filme não encontrado" + error)
     }
 }
+
+// busca por filmes populares 
+const fetchMoviesPopulares = async () => {
+    try{
+        const response = await tmdbApi.get('/movie/popular')
+        console.log("populares" , response.data.results)
+        return response.data.results
+    } catch (error) {
+
+        console.log("nenhum filme popular encontrado")
+    }
+
+}
+
+const renderMovies = async () => {
+    const data = await fetchMoviesPopulares()
+
+    data.forEach(filme => {
+        console.log(filme.title)
+        console.log(filme.backdrop_path)
+        console.log(filme.vote_average)
+        console.log(filme.poster_path)
+        
+        
+        // div img-card
+        const div = document.createElement("div")
+        div.classList.add("movie-card")
+        
+        // div img
+        const divImg = document.createElement("div")
+        divImg.classList.add("movie-img")
+
+        const img = document.createElement("img")
+        divImg.appendChild(img)
+        
+        // div title
+        const divTitle = document.createElement("div")
+        const h3Title = document.createElement("h2")
+        const icon = `<span><ion-icon class="star-icon" name="star"></ion-icon> 4.5</span>`
+
+        // div movi-info
+        const divInfo = document.createElement("div")
+        divInfo.classList.add("movie-info")
+        const spanInfo = document.createElement("span")
+        const infoIcon = `span><ion-icon class="star-icon" name="star"></ion-icon> 4.5</span>`
+        divInfo.appendChild(spanInfo)
+        divInfo.innerHTML = infoIcon
+        console.log(divInfo)
+        
+    });
+}
+
+fetchMoviesPopulares()
+renderMovies()
+
 
 
 
