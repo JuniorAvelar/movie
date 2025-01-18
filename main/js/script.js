11a97dba34eb7e351ca4dba8c030c5801dec6a5a
Edivaldo Junior <junioravelar853@gmail.com>
2025-01-18 10:06:39 -0300

    melhoria
    adicionei um slider horizontal para melhorar a visualização dos filmes

--- main/js/script.js
+++ main/js/script.js
@@ -2,6 +2,7 @@
 const inputSearch = document.querySelector(".search-input")
 const formSearch = document.querySelector(".form-search")
 const cardControl = document.querySelector(".card-control")
+const swiperWrapper = document.querySelector(".swiper-wrapper")
 
 const baseURL = "https://image.tmdb.org/t/p/w780"
 
@@ -53,11 +54,12 @@ const renderMovies = async () => {
     data.forEach(filme => {
         const movieTitle = filme.title
         const movieImg = filme.poster_path
-        const movieQualification = filme.vote_average        
-        
+        const movieQualification = filme.vote_average
+        const movieData = filme.release_date
+
         // div img-card
         const div = document.createElement("div")
-        div.classList.add("movie-card")
+        div.classList.add("movie-card" , 'swiper-slide')
         
         // div img
         const divImg = document.createElement("div")
@@ -65,51 +67,59 @@ const renderMovies = async () => {
 
         const img = document.createElement("img")
         divImg.appendChild(img)
-        img.src = baseURL + filme.poster_path
+        img.src = baseURL + movieImg
         
         // div title
         const divTitle = document.createElement("div")
         divTitle.classList.add("movie-title")
         const h3Title = document.createElement("h3")
-        h3Title.innerText = filme.title
+        h3Title.innerText = movieTitle
         const spanIcon =  document.createElement("span")
-        // spanIcon.innerHTML = `<ion-icon class="movie-card-icon" name="heart-outline"></ion-icon>`
 
         divTitle.appendChild(h3Title)
-        // divTitle.appendChild(spanIcon)
 
         // div movi-info
         const divInfo = document.createElement("div")
         divInfo.classList.add("movie-info")
 
         const spanInfo = document.createElement("span")
-        spanInfo.textContent = filme.
-        release_date
+        spanInfo.textContent = movieData
 
         const infoIcon = document.createElement("span")
         infoIcon.classList.add("span-info")
         infoIcon.innerHTML = '<ion-icon class="star-icon" name="star"></ion-icon>'
 
         const avSpan = document.createElement("span")
-        avSpan.textContent = filme.vote_average
+        avSpan.textContent = movieQualification
 
 
         divInfo.appendChild(spanInfo)
         divInfo.appendChild(infoIcon)
         infoIcon.appendChild(avSpan)
 
-
-
+        // criando card
         div.appendChild(divImg)
         div.appendChild(divTitle)
         div.appendChild(divInfo)
 
-        cardControl.appendChild(div)
+        swiperWrapper.appendChild(div)
+        cardControl.appendChild(swiperWrapper)
 
-        console.log(div)
+        swiper.update();
+        console.log(cardControl)
         
     });
 }
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 fetchMoviesPopulares()
 renderMovies()
